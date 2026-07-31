76e231830372b8e0384ede7c3270f7eb5d57a19a
epili <epili@192.168.10.36>
2026-07-31 12:33:36 +0530

    today new file

---
+++ Core_Java_P/src/com/nt/SwitchCase.java
@@ -0,0 +1,22 @@
+package com.nt;
+
+public class SwitchCase {
+	public static void main(String[] args) {
+		int a =1;
+		String result = switch(a)
+		{
+			case 1->
+			{
+				System.out.println("Apple");
+				System.out.println("mango");
+				yield "A";
+			}
+			case 2-> "B";
+			case 3-> "C";
+			case 4-> "D";
+			case 5-> "E";
+			default -> "Enter valid Input";
+		};
+		System.out.println(result);
+	}
+}

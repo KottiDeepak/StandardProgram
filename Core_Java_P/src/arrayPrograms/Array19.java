e49f34119313e6fa5de94b7fcf42d7089df17faf
epili <epili@10.0.0.115>
2026-09-08 15:52:48 +0530

    initial commit

--- Core_Java_P/src/arrayPrograms/Array19.java
+++ Core_Java_P/src/arrayPrograms/Array19.java
@@ -1,17 +1,16 @@
-package arrayPrograms;System.out.println("ente the size of the array");
+package arrayPrograms;
+import java.util.Scanner;
+
+public class Array19 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("ente the size of the array");
 		int size = sc.nextInt();
 		int a[] = new int[size];
 		System.out.println("enter the array elements ");
 		for (int i = 0; i < a.length; i++) {
 			a[i] = sc.nextInt();
 		}
-
-import java.util.Scanner;
-
-public class Array19 {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
 		//boolean isAnyElementRepeated = false;
 		for (int i = 0; i < a.length - 1; i++) {
 			int count = 0;
